2dae598f234c99bc33cca63b3ee86390e58d3544
Sergio Carey <104459403+SC-Sergio@users.noreply.github.com>
2022-07-20 00:37:12 -0400

    Add files via upload

---
+++ MySQL.cs
@@ -0,0 +1,23 @@
+﻿using MySql.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MysqlProject.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+    }
+
+    public class PersonaContext : DbContext
+    {
+        public DbSet<Persona> Persona { get; set; }
+
+        protected override void  OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseMySQL("server=localhost;database=dbvisualstudio;user=root;password=jajaime89");
+        }
+    }
+}
---
+++ SQLServer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Test_Sabado.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+
+    }
+    public class PersonaDbContext : DbContext
+    {
+        public DbSet<Persona> Persona { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                 @"Server=localhost;Database=test;Trusted_Connection=True");
+        }
+    }
+}
